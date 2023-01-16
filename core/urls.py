b8d8324af363d6c84924dbd03add841f5ef24902
nithindaniel1 <nithindaniel2018@gmail.com>
2023-01-16 21:01:00 +0530

    signup funtionality added

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('register/',views.register,name="register"),
+    path('signin/',views.signin,name="signin")
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/signup.html')
+    return render(request, 'core/index.html')
 
 
 def register(request):
     if request.method == 'POST':
-        full_name = request.POST['first_name']
+        first_name = request.POST['first_name']    
+        last_name = request.POST['last_name']  
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
+        email = request.POST['email']
 
-        user = User.objects.create_user(username=username,password1=password1,password2=password2)
-        user.save()
-        print('user created')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email already exists ' )
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,password=password2,email=email,first_name=first_name,last_name=last_name)
+                user.save()
+                
+                return redirect('signin')
+        else:
+            messages.info(request, 'password not matching......')
+            return redirect('register') 
+
+    else:    
+            return render(request, 'core/signup.html')
+
+
+def signin(request):
+    return render(request, 'core/signin.html')
